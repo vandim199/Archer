b953e53a06a8bc2cf59d9a4167883f27e9e7592e
vandim199 <vandim@abv.bg>
2021-04-29 11:43:51 +0300

    Button2

---
+++ GXPEngine/GXPEngine/Button.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    public class Button : Sprite
+    {
+        public Button(float x, float y):base("square.png")
+        {
+            SetXY(x, y);
+        }
+
+        void Update()
+        {
+            
+        }
+
+        public bool Click()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (HitTestPoint(Input.mouseX, Input.mouseY))
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+    }
+}
--- GXPEngine/GXPEngine/MyGame.cs
+++ GXPEngine/GXPEngine/MyGame.cs
@@ -13,8 +13,47 @@ public class MyGame : Game
     ColliderManager colliderManager = new ColliderManager();
     Player player;
 
+    Button startButton;
+
 	public MyGame() : base(1920, 1080, false)		// Create a window that's 800x600 and NOT fullscreen
 	{
+        LoadMenu();
+    }
+
+    void Update()
+	{
+        if (startButton.Click())
+        {
+            startButton.LateDestroy();
+            LoadGame();
+        }
+
+        if (cam != null)
+        {
+            if (Input.GetKey(Key.ZERO) && cam.scale >= 0.6f)
+            {
+                cam.scale -= 0.2f;
+            }
+            if (Input.GetKey(Key.NINE) && cam.scale <= 3)
+            {
+                cam.scale += 0.2f;
+            }
+        }
+    }
+
+	static void Main()							// Main() is the first method that's called when the program is run
+	{
+		new MyGame().Start();					// Create a "MyGame" and start it
+	}
+
+    void LoadMenu()
+    {
+        startButton = new Button(300, 300);
+        AddChild(startButton);
+    }
+
+    void LoadGame()
+    {
         player = new Player();
         AddChild(player);
 
@@ -23,41 +62,20 @@ public class MyGame : Game
         player.AddChild(cam);
 
         //lines.Add(new LineSegment(this, 0, 0, width, 0));
-        lines.Add(new LineSegment(this, 0, 510, 0, 0));
+        lines.Add(new LineSegment(this, 0, 510, 0, 200));
         lines.Add(new LineSegment(this, width, 0, width, height));
-        lines.Add(new LineSegment(this, 300, 500, -10, 500, newFloor:true));
+        lines.Add(new LineSegment(this, 300, 500, -10, 500, newFloor: true));
         lines.Add(new LineSegment(this, 300, 600, 300, 500));
-        lines.Add(new LineSegment(this, 850, 500, 500, 500, newFloor:true));
+        lines.Add(new LineSegment(this, 850, 500, 500, 500, newFloor: true));
         lines.Add(new LineSegment(this, 500, 500, 500, 600));
         lines.Add(new LineSegment(this, 800, 1000, 0, 1000, newFloor: true));
-        
-        lines.Add(new LineSegment(this, 1200, 420, 800, 510, newFloor:true));
-        //lines.Add(new LineSegment(this, 400, 450, 0, 400, newFloor: true));
 
-        //AddChild(new LineCollider(new Vec2(300, 500), new Vec2(0, 500), newLineWidth: 1));
+        lines.Add(new LineSegment(this, 1200, 420, 800, 510, newFloor: true));
+        //lines.Add(new LineSegment(this, 400, 450, 0, 400, newFloor: true));
 
         //AddChild(new Box());
 
         foreach (LineSegment line in lines) AddChild(line);
         foreach (Ball ball in balls) AddChild(ball);
-
-        
     }
-
-    void Update()
-	{
-        if (Input.GetKey(Key.ZERO) && cam.scale >= 0.6f)
-        {
-            cam.scale -= 0.2f;
-        }
-        if (Input.GetKey(Key.NINE) && cam.scale <= 3)
-        {
-            cam.scale += 0.2f;
-        }
-    }
-
-	static void Main()							// Main() is the first method that's called when the program is run
-	{
-		new MyGame().Start();					// Create a "MyGame" and start it
-	}
 }
